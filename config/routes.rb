cbf523d64770f29260fec256319364cca087fa5d
leahkhaneeva <liyahaneeva@gmail.com>
2021-10-23 08:31:20 +0300

    Popytka

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  resources :subscribers
+  get 'about/index'
+  get 'promo/index'
+  resources :posts do
+    resources :comments
+  end
+  get 'posts/index'
+
+  get 'promo', to: 'promo#index'
+  get 'about', to: 'about#index'
+
+  root 'promo#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20211022134638_remove_comments_from_post.rb
@@ -0,0 +1,4 @@
+class RemoveCommentsFromPost < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
